a43080d6b548b185a75bd8736f3a26aa7844adfc
Saimon Michelson <saimon.michelson@gmail.com>
2025-04-16 12:50:11 -0400

    add list dir mcp tool

--- src/mcp_ctera.py
+++ src/mcp_ctera.py
@@ -10,6 +10,17 @@ from cterasdk import (
 )
 
 
+from cterasdk.core.files.common import (
+    get_object_path,
+    FetchResourcesParamBuilder,
+    FetchResourcesResponse
+)
+
+from cterasdk.asynchronous.core import (
+    query
+)
+
+
 from mcp.server.fastmcp import Context, FastMCP
 
 
@@ -53,6 +64,25 @@ async def ctera_who_am_i(ctx: Context) -> str:
         username = f'{username}@{session.domain}'
     
     return f'Authenticated as {username}'
+
+
+@mcp.tool()
+async def ctera_listdir(path: str, search_criteria: str = None, ctx: Context = None) -> list[str]:
+    """
+    List the contents of a specified directory.
+
+    Args:
+        path (str): The path to the directory to list.
+
+    Returns:
+        List[str]: A list of file and subdirectory names within the specified directory.
+    """
+    user = ctx.request_context.lifespan_context.user
+    builder = FetchResourcesParamBuilder().root(get_object_path('/ServicesPortal/webdav', path).encoded_fullpath()).depth(1)
+    if search_criteria:
+        builder.searchCriteria(search_criteria)
+
+    return [e.name async for e in query.iterator(user, '', builder.build(), 'fetchResources', callback_response=FetchResourcesResponse)]
 
 
 if __name__ == "__main__":
